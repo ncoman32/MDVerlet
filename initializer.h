2292952e7eaee4ed8d56f6f8d52987eae3b54324
Coman Nicu <ncoman32@yahoo.com>
2019-11-02 16:27:02 +0200

    Verlet Optimized Method

---
+++ initializer.h
@@ -0,0 +1,29 @@
+//
+// Created by nicu on 16.10.2019.
+//
+
+#ifndef MDNOTOPTIMIZED_INITIALIZER_H
+#define MDNOTOPTIMIZED_INITIALIZER_H
+
+#endif //MDNOTOPTIMIZED_INITIALIZER_H
+//  initializer.h
+//  Simulation Methods course, 2018
+//  First Assignment: Molecular Dynamics (Brownian Dynamics) Simulation
+
+#ifndef initializer_h
+#define initializer_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_simulation(void);
+void init_simulation_box(void);
+
+void init_particles(void);
+void init_particles_randomly(void);
+
+void init_files(void);
+
+double distance_folded_PBC(double x0,double y0,double x1,double y1);
+
+#endif /* initializer_h */
---
+++ timing.h
@@ -0,0 +1,15 @@
+//
+// Created by nicu on 16.10.2019.
+//
+
+#ifndef MDNOTOPTIMIZED_TIMING_H
+#define MDNOTOPTIMIZED_TIMING_H
+
+#endif //MDNOTOPTIMIZED_TIMING_H
+#pragma once
+#include <time.h>
+
+
+void get_starting_time(void);
+void get_finishing_time(void);
+void echo_running_time(void);
